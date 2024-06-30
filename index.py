0a37ca93fcb2172f7985838e4b30df1753c38510
ujwal1hero <168129796+ujwal1hero@users.noreply.github.com>
2024-06-30 19:11:43 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+# print_array.py
+
+def print_array_values(array):
+    for value in array:
+        print(value)
+
+# Example array
+example_array = [1, 2, 3, 4, 5]
+
+# Call the function with the example array
+print_array_values(example_array)
